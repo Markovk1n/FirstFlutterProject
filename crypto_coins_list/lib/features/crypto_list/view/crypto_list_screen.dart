9095e26242e99a661a2d66bf1e62c87b45efbfc7
Mark <roalk733@gmail.com>
2023-10-18 22:28:32 +0600

    added models/repository, API from cryptocompare

--- crypto_coins_list/lib/features/crypto_list/view/crypto_list_screen.dart
+++ crypto_coins_list/lib/features/crypto_list/view/crypto_list_screen.dart
@@ -1,6 +1,7 @@
 
+import 'package:crypto_coins_list/repositories/crypto_coins/crypto_coins_repository.dart';
+import 'package:crypto_coins_list/repositories/crypto_coins/models/crypto_coin.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 
 import '../widgets/widgets.dart';
 
@@ -11,7 +12,14 @@ class CryptoListScreen extends StatefulWidget {
 }
 
 class _CryptoListScreenState extends State<CryptoListScreen> {
- 
+  
+  List<CryptoCoin>? _cryptoCoinsList;
+  @override
+  void initState() {
+    _loadCryptoCoins();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
    final theme = Theme.of(context);
@@ -21,16 +29,30 @@ class _CryptoListScreenState extends State<CryptoListScreen> {
         leading:  Icon(Icons.arrow_back),
       ),
 
-      body: ListView.separated(
-        itemCount: 10,
-        separatorBuilder: (context, index) => const Divider(),
-        
-        itemBuilder: (context, i) {
-          const coinName = 'Bitcoin';
-        return  const CryptoCointTile(coinName: coinName);
+      body: (_cryptoCoinsList == null) 
+      ? const Center(child: const CircularProgressIndicator())
+      : ListView.separated(
+          padding: const EdgeInsets.only(top:16),
+          itemCount: _cryptoCoinsList!.length,
+          separatorBuilder: (context, index) => const Divider(),
+          itemBuilder: (context, i) {
+            final coin = _cryptoCoinsList![i];
+          return CryptoCointTile(coin: coin);
         },
       ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.download),
+        onPressed: ()async{
+          await _loadCryptoCoins();
+      }),
     );
+  }
+
+  Future<void> _loadCryptoCoins() async {
+    _cryptoCoinsList = await CryptoCoinsRepository().getCoinsList();
+    setState(() {
+      
+    });
   }
 }
 
