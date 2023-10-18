e13bcb4777e765556a390257689e92b25b0a7bbb
Mark <roalk733@gmail.com>
2023-10-18 20:44:55 +0600

    finaly refactored / new imports

---
+++ crypto_coins_list/lib/crypto_coins_list_app.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter/material.dart';
+import 'router/router.dart';
+import 'theme/theme.dart';
+
+class CryptoCurrenciesApp extends StatelessWidget {
+  const CryptoCurrenciesApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CryptoCurrenciestList',
+      theme: darkTheme,
+      routes: routes,
+    );
+  }
+}
+
---
+++ crypto_coins_list/lib/features/crypto_coin/view/crypto_coin_screen.dart
@@ -0,0 +1,43 @@
+
+import 'package:flutter/material.dart';
+
+class CryptoCoinScreen extends StatefulWidget {
+  const CryptoCoinScreen({super.key});
+
+  @override
+  State<CryptoCoinScreen> createState() => _CryptoCoinScreenState();
+}
+
+class _CryptoCoinScreenState extends State<CryptoCoinScreen> {
+  String? coinName;
+
+  @override
+  void didChangeDependencies() {
+    final args = ModalRoute.of(context)?.settings.arguments;
+    assert(args != null && args is String, 'You must provide String args');
+
+    // if (args ==null){
+    //   print('You must write args');
+    //   return;
+    // }
+
+    // if (args is! String){
+    //   log('You must provide String args' as num);
+    //   return;
+    // }
+
+    coinName = args as String;
+    setState(() {
+      
+    });
+    super.didChangeDependencies();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(coinName ?? '...'),),
+    );
+  }
+}
